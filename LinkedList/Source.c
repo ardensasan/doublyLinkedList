34aa816f51157638776d696f601b91d2b6bf04f8
Arden Sasan <ardensasan@gmail.com>
2020-12-15 17:57:01 +0800

    added choice to create new simple linked list
    fixed a bug where no items are displayed when display more than one time

--- LinkedList/Source.c
+++ LinkedList/Source.c
@@ -1,17 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 typedef struct node {
 	int data;
 	struct Node* next;
 	struct Node* prev;
 }Node;
 
-int main() {
+void CreateSimpleLinkedList(int num) {
+	Node* head = NULL;
+	for (int i = 0;i < num;i++) {
+		int data;
+		printf("Node number %d data: ", i + 1);
+		scanf_s("%d", &data);
+		Node* temp = (Node*)malloc(sizeof(Node));
+		if (head == NULL) {
+			temp->data = data;
+			temp->next = NULL;
+			head = temp;
+		}
+		else {
+			temp->data = data;
+			temp->next = head;
+			head = temp;
+		}
+	}
+	simpleLinkListDisplay:
+		system("cls");
+		printf("Simple Link List \n 1. New Simple Link List\n 2. Display Items in List\n 3. Exit\n");
+		int choice = 0;
+		int numNodes = 0;
+		scanf_s("%d", &choice);
+		switch (choice)
+		{
+		case 1:
+			printf("\nType: Simple Linked List");
+			printf("\nEnter number of nodes: ");
+			scanf_s("%d", &numNodes);
+			CreateSimpleLinkedList(numNodes);
+		case 2:
+			printf("\nItems in the linked list are\n");
+			Node* currentNode = head;
+			while (currentNode != NULL) {
+				printf("%d  ", currentNode->data);
+				currentNode = currentNode->next;
+			}
+			_getch();
+			break;
+		case 3:
+			exit(0);
+			break;
+		default:
+			break;
+		}
+	goto simpleLinkListDisplay;
+	return;
+}
+
+void CreateDoublyLinkedList(int num) {
 	Node* head = NULL;
 	Node* tail = NULL;
-	int num;
-	printf("Enter number of nodes: ");
-	scanf_s("%d", &num);
 	for (int i = 0;i < num;i++) {
 		int data;
 		printf("Node number %d data: ", i + 1);
@@ -37,8 +85,34 @@ int main() {
 			tail->next = NULL;
 		}
 	}
+	printf("\D ");
 	while (head != NULL) {
-		printf("%d", head->data);
+		printf("%d  ", head->data);
 		head = head->next;
 	}
+	return;
+}
+int main() {
+	int type;
+	int numNodes = 0;
+	printf("Linked List of Numbers\nChoose a linked list type: \n 1. Simple Linked List\n 2. Doubly Linked List\n 3. Exit\n");
+	scanf_s("%d", &type);
+	switch (type) {
+	case 1:
+		printf("\nType: Simple Linked List");
+		printf("\nEnter number of nodes: ");
+		scanf_s("%d", &numNodes);
+		CreateSimpleLinkedList(numNodes);
+		break;
+	case 2:
+		printf("\nType: Doubly Linked List");
+		printf("\nEnter number of nodes: ");
+		scanf_s("%d", &numNodes);
+		CreateDoublyLinkedList(numNodes);
+		break;
+	case 3:
+		exit(0);
+		break;
+	}
+	return 0;
 }
