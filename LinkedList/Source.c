e9adea7a872cce22ab44ad1cdc074bd512448728
Arden Sasan <ardensasan@gmail.com>
2020-12-18 14:48:58 +0800

    added feature to insert element from head

--- LinkedList/Source.c
+++ LinkedList/Source.c
@@ -10,34 +10,36 @@ struct node {
 
 void SimpleLinkedList(int num) {
 	Node* head = NULL;
+	Node* tail = NULL;
 	for (int i = 0;i < num;i++) {
 		int data;
 		printf("Node number %d data: ", i + 1);
 		scanf_s("%d", &data);
 		Node* temp = malloc(sizeof(Node));
-		if (temp)
-		{
-			if (head == NULL) {
-				temp->data = data;
-				temp->next = NULL;
-				head = temp;
-			}
-			else {
-				temp->data = data;
-				temp->next = head;
-				head = temp;
-			}
+		if (head == NULL) {
+			temp->data = data;
+			temp->next = NULL;
+			head = temp;
+			tail = temp;
 		}
 		else {
-			printf("Malloc Failed to Allocate Memory");
-			exit(0);
+			temp->data = data;
+			if (head->next == NULL) {
+				head->next = temp;
+			}
+			else {
+				tail->next = temp;
+			}
+			tail = temp;
+			tail->next = NULL;
 		}
 	}
 	simpleLinkListDisplay:
 		system("cls");
-		printf("Simple Link List \n 1. New Simple Link List\n 2. Display Items in List\n 3. Return\n ");
+		printf("Simple Link List \n 1. New Simple Link List\n 2. Display Items in List\n 3. Insert element\n 4. Return\n ");
 		int choice = 0;
 		int numNodes = 0;
+		int element = 0;
 		scanf_s("%d", &choice);
 		switch (choice)
 		{
@@ -56,6 +58,23 @@ void SimpleLinkedList(int num) {
 			_getch();
 			break;
 		case 3:
+			printf("\n 1. Insert element from head\n 2. Insert element from tail \n 3. Insert element to index \n");
+			scanf_s("%d", &choice);
+			switch (choice) {
+			case 1:
+				printf("\n Enter element to insert from head: ");
+				scanf_s(" %d", &element);
+				Node* temp = malloc(sizeof(Node));
+				temp->data = element;
+				temp->next = head;
+				head->prev = temp;
+				head = temp;
+				printf("\n Element successfully added");
+				_getch();
+				goto simpleLinkListDisplay;
+			}
+			break;
+		case 4:
 			return;
 			break;
 		default:
@@ -97,9 +116,11 @@ void DoublyLinkedList(int num) {
 	}
 doublyLinkListDisplay:
 	system("cls");
-	printf("Doubly Link List \n 1. New Doubly Link List\n 2. Display Items in List From Head\n 3. Display Items in List From Tail\n 4. Return\n ");
+	printf("Doubly Link List \n 1. New Doubly Link List\n 2. Display Items in List From Head\n 3. Display Items in List From Tail\n 4. Insert element\n 5. Return\n ");
 	int choice = 0;
+	int element = 0;
 	int numNodes = 0;
+	int count = 0;
 	scanf_s("%d", &choice);
 	switch (choice)
 	{
@@ -127,6 +148,24 @@ doublyLinkListDisplay:
 		_getch();
 		break;
 	case 4:
+		printf("\n 1. Insert element from head\n 2. Insert element from tail \n 3. Insert element to index \n");
+		scanf_s("%d", &choice);
+		switch (choice) {
+		case 1:
+			printf("\n Enter element to insert from head: ");
+			scanf_s(" %d", &element);
+			Node* temp = malloc(sizeof(Node));
+			temp->data = element;
+			temp->next = head;
+			temp->prev = NULL;
+			head->prev = temp;
+			head = temp;
+			printf("\n Element successfully added");
+			_getch();
+			goto doublyLinkListDisplay;
+		}
+		break;
+	case 5:
 		return;
 		break;
 	default:
